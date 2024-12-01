46f593e34d06b52495991b6eee994e8ccd11a55b
aaronjpotter93 <aaronjpotter93@gmail.com>
2024-11-30 17:35:25 -0700

    finished section 13 of Udemy Course

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +12,33 @@ export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'
 
+  private categoryUrl = 'http://localhost:8080/api/product-category'
+
   constructor(private htttpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.htttpClient.get<GetResponse>(searchUrl).pipe(
+    return this.htttpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    )
+  }
+
+  getProductCategories() {
+    return this.htttpClient.get<GetResponseCategories>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     )
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseCategories {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
